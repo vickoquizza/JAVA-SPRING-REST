dfc10731747b0a5c4c14960e75b6f7ff177feff9
vickoquizza <97573852+vickoquizza@users.noreply.github.com>
2022-02-01 15:27:35 -0500

    Add files via upload

---
+++ week2/SpringBoot/SprinBootCurso1/src/main/java/com/vicko/SprinBootCurso1/SprinBootCurso1Application.java
@@ -0,0 +1,13 @@
+package com.vicko.SprinBootCurso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootCurso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootCurso1Application.class, args);
+	}
+
+}
---
+++ week2/SpringBoot/SprinBootCurso1/src/test/java/com/vicko/SprinBootCurso1/SprinBootCurso1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicko.SprinBootCurso1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootCurso1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
