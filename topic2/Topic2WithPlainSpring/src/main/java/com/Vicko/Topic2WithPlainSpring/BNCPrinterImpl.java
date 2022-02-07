f6ad83ce55acd52d73bdc56900d2292de2a4b064
vickoquizza <97573852+vickoquizza@users.noreply.github.com>
2022-02-07 15:16:15 -0500

    Delete BNCPrinterImpl.java

--- topic2/Topic2WithPlainSpring/src/main/java/com/Vicko/Topic2WithPlainSpring/BNCPrinterImpl.java
+++
@@ -1,8 +0,0 @@
-package com.Vicko.Topic2WithPlainSpring;
-
-public class BNCPrinterImpl implements Printer{
-    @Override
-    public String print() {
-        return "Imprimiendo en una impresora con Blanco,Negro y Color";
-    }
-}
