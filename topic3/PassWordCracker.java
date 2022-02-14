d53d935027e779863b6d5d62aaa80eb99a65c00d
vickoquizza <97573852+vickoquizza@users.noreply.github.com>
2022-02-14 17:27:20 -0500

    Add files via upload

--- topic3/PassWordCracker.java
+++ topic3/PassWordCracker.java
@@ -1,33 +1,40 @@
 package Topic3;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PassWordCracker {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<String> passwords = new ArrayList<>();
-        String passwordAttempt = "";
+        List<String> passwords;
+        String passwordAttempt;
 
         System.out.println("passwords");
         passwords = Arrays.asList(scan.nextLine().toLowerCase().split("\\s+"));
         System.out.println("Password attempt");
         passwordAttempt = scan.nextLine().toLowerCase();
 
-        String coincidences = passWordCracker(passwords, passwordAttempt);
+        List<String> coincidences = passWordCracker(passwords, passwordAttempt);
 
+        String coincidencesString  = coincidences.stream().collect(Collectors.joining(" "));
 
-        System.out.println(coincidences);
+        if (passwords.stream().allMatch(password -> coincidences.contains(password))){
+            System.out.println(coincidencesString);
+        }else{
+            System.out.println("WRONG PASSWORD");
+        }
 
 
 
     }
 
-   static String passWordCracker(List<String> passwords, String passwordAttempt){
+   static List<String> passWordCracker(List<String> passwords, String passwordAttempt){
        return passwords.stream()
                .filter(passwordAttempt::startsWith)
-               .map(password -> passwordAttempt.startsWith(password) ? passWordCracker(passwords, passwordAttempt.substring(password.length())) : "False")
-               .collect(Collectors.joining(" "));
-    };
+               .flatMap(password -> Stream.concat(Stream.of(password), passWordCracker(passwords, passwordAttempt.substring(password.length())).stream()))
+               .collect(Collectors.toList());
+    }
+
+
 }
