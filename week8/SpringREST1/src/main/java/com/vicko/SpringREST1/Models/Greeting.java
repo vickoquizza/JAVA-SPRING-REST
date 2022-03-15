cacebafd288c7200de65b0a4ccc3a31678809eb2
vickoquizza <97573852+vickoquizza@users.noreply.github.com>
2022-03-15 15:00:20 -0500

    Add files via upload

---
+++ week8/SpringREST1/src/main/java/com/vicko/SpringREST1/Models/Greeting.java
@@ -0,0 +1,20 @@
+package com.vicko.SpringREST1.Models;
+
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content){
+        this.id =  id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
